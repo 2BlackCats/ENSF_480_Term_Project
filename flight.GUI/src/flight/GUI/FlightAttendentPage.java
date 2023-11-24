4ae0a8151e7e9461eae281fd1c29636c564770a6
2BlackCats <903036002alex@gmail.com>
2023-11-24 15:12:02 -0700

    Template code for GUI

---
+++ flight.GUI/src/flight/GUI/FlightAttendentPage.java
@@ -0,0 +1,65 @@
+package flight.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JToggleButton;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class FlightAttendentPage {
+
+	private JFrame frame;
+	private JTable table;
+	private JTable table_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FlightAttendentPage window = new FlightAttendentPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FlightAttendentPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		
+		JSplitPane splitPane = new JSplitPane();
+		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		//Table for Flights
+		table = new JTable();
+		splitPane.setLeftComponent(table);
+		
+		//Table for seats
+		table_1 = new JTable();
+		splitPane.setRightComponent(table_1);
+	
+	}
+
+}
